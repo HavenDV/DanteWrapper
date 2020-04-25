78b41a2806dfa34b092f4b48081315110b3d8b00
Konstantin S <havendv@gmail.com>
2020-04-25 16:28:10 +1000

    Initial commit

---
+++ DanteWrapperLibrary.Tests/DanteBrowsingTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DanteWrapperLibrary.Tests
+{
+    [TestClass]
+    public class DanteBrowsingTests
+    {
+        [TestMethod]
+        public void GetNamesTest()
+        {
+            foreach (var name in DanteBrowsing.GetNames())
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ DanteWrapperLibrary/DanteBrowsing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DanteWrapperLibrary
+{
+    public class DanteBrowsing
+    {
+        [DllImport("../../../Dependencies/dante_browsing_test.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int test(int argc, [In, Out] string[] argv);
+
+        public static IList<string> GetNames()
+        {
+            var args = new[] { "hello.exe", "-conmon", "", "", "", "", "", "", "", "" };
+            test(2, args);
+
+            return args;
+        }
+    }
+}
