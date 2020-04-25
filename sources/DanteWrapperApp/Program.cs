3e849af39e218c44f987b0baec66a378d928ac48
Konstantin S <havendv@gmail.com>
2020-04-25 23:31:10 +1000

    Added ConsoleApp

---
+++ sources/DanteWrapperApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DanteWrapperLibrary;
+
+namespace DanteWrapperApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            foreach (var name in DanteRouting.GetRxChannels("WIN-59B9F8QNN58"))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
