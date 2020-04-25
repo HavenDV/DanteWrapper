9325964662c413ca3ff4cf60e5b038f5c31aab83
Konstantin S <havendv@gmail.com>
2020-04-25 19:10:41 +1000

    Added DanteRouting template

---
+++ sources/DanteWrapperLibrary/DanteRouting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DanteWrapperLibrary
+{
+    public class DanteRouting
+    {
+        [DllImport("dante_browsing_test.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int GetNames(
+            out IntPtr stringArray,
+            out int count
+        );
+
+        public static IList<string> GetNames()
+        {
+            var result = GetNames(out var ptr, out var count);
+            if (result != 0)
+            {
+                throw new InvalidOperationException($"Bad result: {result}");
+            }
+
+            MarshalUtilities.ToManagedStringArray
+            (
+                ptr,
+                count,
+                out var array
+            );
+
+            return array;
+        }
+    }
+}
